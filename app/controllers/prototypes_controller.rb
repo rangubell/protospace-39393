ca9ab63d66abd2de043a83aaba65cb4794290086
rangubell <rangubell@icloud.com>
2023-06-07 18:15:36 +0900

    新規投稿時のエラー修正②

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   
   def index
     @prototypes = Prototype.includes(:user)
